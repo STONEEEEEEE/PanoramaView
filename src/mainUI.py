77e29e7607b7dfd7b02e245286e42a6f1c23d5fa
STONEEE <864129884@qq.com>
2023-05-24 16:04:11 +0800

    更新完善UI

--- src/mainUI.py
+++ src/mainUI.py
@@ -1,6 +1,7 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtWidgets import QGraphicsScene, QGraphicsPixmapItem
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QGraphicsPixmapItem, QGraphicsScene
+from PyQt5.QtCore import Qt
 
 from GUI import Ui_MainWindow
 
@@ -10,20 +11,123 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
+        self.reInit()
 
 
+    def reInit(self):
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setMinimumSize(QtCore.QSize(450, 450))
+        self.label.setStyleSheet("border-image:url(:/map/map.jpg)")
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1)
+
+        # 设定全景框参数
+        self.disp_w = 757
+        self.disp_h = 552
+        self.pos_horz = 0
+        self.pos_vert = 0
+
+        # 为方向键绑定事件
+        self.pushButton.clicked.connect(self.leftMove)
+        self.pushButton_2.clicked.connect(self.downMove)
+        self.pushButton_3.clicked.connect(self.rightMove)
+        self.pushButton_4.clicked.connect(self.upMove)
 
 
+    def mousePressEvent(self, event):  # 鼠标按下事件
+        pos = event.pos()  # 返回鼠标所在点QPoint
+        self.statusBar.clearMessage()
+        self.statusBar.showMessage('Mouse is pressed at (%d,%d) of widget ' % (pos.x(), pos.y()), 0)
+        print('Mouse is pressed at (%d,%d) of screen ' % (pos.x(), pos.y()))
+        if 80 <= pos.x() <= 125 and 10 <= pos.y() <= 35:
+            self.pix = QPixmap("..\\data\\srcfile\\zhuxuan1.jpg")
+            img_w = self.pix.width()
+            img_h = self.pix.height()
+            self.label_2.setFixedSize(self.disp_w, self.disp_h)
+            self.verticalScrollBar.setRange(0, img_h - self.disp_h)
+            self.verticalScrollBar.valueChanged.connect(self.vertPosChanged)
+            self.horizontalScrollBar.setRange(0, img_w - self.disp_w)
+            self.horizontalScrollBar.setFixedWidth(self.disp_w)
+            self.horizontalScrollBar.valueChanged.connect(self.horzPosChanged)
+            # 状态栏提示
+            self.statusBar.showMessage("当前位置：校区一号门")
+            # 初始化图片位置
+            self.label_2.setPixmap(self.pix.copy(self.pos_horz, self.pos_vert, self.disp_w, self.disp_h))
+
+        elif 190 <= pos.x() <= 240 and 140 <= pos.y() <= 175:
+            self.pix = QPixmap("..\\data\\srcfile\\zhuxuan2.jpg")
+            img_w = self.pix.width()
+            img_h = self.pix.height()
+            self.label_2.setFixedSize(self.disp_w, self.disp_h)
+            self.verticalScrollBar.setRange(0, img_h - self.disp_h)
+            self.verticalScrollBar.valueChanged.connect(self.vertPosChanged)
+            self.horizontalScrollBar.setRange(0, img_w - self.disp_w)
+            self.horizontalScrollBar.setFixedWidth(self.disp_w)
+            self.horizontalScrollBar.valueChanged.connect(self.horzPosChanged)
+            # 状态栏提示
+            self.statusBar.showMessage("当前位置：中山图书馆")
+            # 初始化图片位置
+            self.label_2.setPixmap(self.pix.copy(self.pos_horz, self.pos_vert, self.disp_w, self.disp_h))
+
+        elif 195 <= pos.x() <= 235 and 205 <= pos.y() <= 230:
+            self.pix = QPixmap("..\\data\\srcfile\\zhuxuan3.jpg")
+            img_w = self.pix.width()
+            img_h = self.pix.height()
+            self.label_2.setFixedSize(self.disp_w, self.disp_h)
+            self.verticalScrollBar.setRange(0, img_h - self.disp_h)
+            self.verticalScrollBar.valueChanged.connect(self.vertPosChanged)
+            self.horizontalScrollBar.setRange(0, img_w - self.disp_w)
+            self.horizontalScrollBar.setFixedWidth(self.disp_w)
+            self.horizontalScrollBar.valueChanged.connect(self.horzPosChanged)
+            # 状态栏提示
+            self.statusBar.showMessage("当前位置：静湖")
+            # 初始化图片位置
+            self.label_2.setPixmap(self.pix.copy(self.pos_horz, self.pos_vert, self.disp_w, self.disp_h))
+
+        elif 250 <= pos.x() <= 290 and 360 <= pos.y() <= 390:
+            self.pix = QPixmap("..\\data\\srcfile\\zhuxuan4.jpg")
+            img_w = self.pix.width()
+            img_h = self.pix.height()
+            self.label_2.setFixedSize(self.disp_w, self.disp_h)
+            self.verticalScrollBar.setRange(0, img_h - self.disp_h)
+            self.verticalScrollBar.valueChanged.connect(self.vertPosChanged)
+            self.horizontalScrollBar.setRange(0, img_w - self.disp_w)
+            self.horizontalScrollBar.setFixedWidth(self.disp_w)
+            self.horizontalScrollBar.valueChanged.connect(self.horzPosChanged)
+            # 状态栏提示
+            self.statusBar.showMessage("当前位置：操场山顶")
+            # 初始化图片位置
+            self.label_2.setPixmap(self.pix.copy(self.pos_horz, self.pos_vert, self.disp_w, self.disp_h))
 
 
-        frame = QImage("..\\img\\zhuxuan.jpg")
-        pix = QPixmap.fromImage(frame)
-        item = QGraphicsPixmapItem(pix)
-        scene = QGraphicsScene()
-        scene.addItem(item)
-        self.graphicsView.setScene(scene)
-        self.graphicsView.show()
+    def horzPosChanged(self, pos):
+        self.pos_horz = pos
+        self.label_2.setPixmap(self.pix.copy(self.pos_horz, self.pos_vert, self.disp_w, self.disp_h))
 
+    def vertPosChanged(self, pos):
+        self.pos_vert = pos
+        self.label_2.setPixmap(self.pix.copy(self.pos_horz, self.pos_vert, self.disp_w, self.disp_h))
+
+    def rightMove(self):
+        if self.pos_horz + self.disp_w <= self.pix.width():
+            self.pos_horz = self.pos_horz + 10
+            self.label_2.setPixmap(self.pix.copy(self.pos_horz, self.pos_vert, self.disp_w, self.disp_h))
+
+    def leftMove(self):
+        if self.pos_horz >= 10:
+            self.pos_horz = self.pos_horz - 10
+            self.label_2.setPixmap(self.pix.copy(self.pos_horz, self.pos_vert, self.disp_w, self.disp_h))
+
+    def upMove(self):
+        if self.pos_vert >= 10:
+            self.pos_vert = self.pos_vert - 10
+            self.label_2.setPixmap(self.pix.copy(self.pos_horz, self.pos_vert, self.disp_w, self.disp_h))
+
+    def downMove(self):
+        if self.pos_vert + self.disp_h <= self.pix.height():
+            self.pos_vert = self.pos_vert + 10
+            self.label_2.setPixmap(self.pix.copy(self.pos_horz, self.pos_vert, self.disp_w, self.disp_h))
 
 
 if __name__ == '__main__':
